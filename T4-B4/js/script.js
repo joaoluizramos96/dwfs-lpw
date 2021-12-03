4e3a8750221549a67a58473ef7fbab8744f9c615
Joao Luiz <joaoluizramos96@gmail.com>
2021-12-03 01:33:38 -0300

    Adição do Tema 4 e seus blocos.

---
+++ T4-B4/js/script.js
@@ -0,0 +1,11 @@
+document.querySelector('#h1element').innerHTML = "Texto no h1";
+document.querySelector('.h2element').innerHTML = "Texto no h2";
+document.querySelector('h3').innerHTML = "Texto no h3";
+
+function alerta(){
+    alert("Alerta");
+}
+
+function consolepessoal(){
+    console.log('Alerta console');
+}
